4728e28ae66ccfa8051e50c39b2d2c7aaeaa66ec
GooseCapital <dungat68@wru.vn>
2020-04-29 16:18:26 +0700

    fix multiaple sensor

--- sensor.py
+++ sensor.py
@@ -4,7 +4,7 @@ from . import getdataevn
 
 import voluptuous as vol
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import (CONF_DISPLAY_OPTIONS, CONF_TYPE, CONF_SCAN_INTERVAL, CONF_USERNAME)
+from homeassistant.const import (CONF_DISPLAY_OPTIONS, CONF_TYPE, CONF_SCAN_INTERVAL, CONF_USERNAME, CONF_NAME)
 from homeassistant.util import Throttle
 from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
@@ -13,6 +13,7 @@ _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "evnhanoi"
 customerId = ''
+name = ''
 SENSOR_TYPES = {
     'customerId': ['Mã khách hàng', 'mdi:account-search'],
     'month': ['Tháng', 'mdi:account-search'],
@@ -26,6 +27,8 @@ SCAN_INTERVAL = datetime.timedelta(seconds=300)  # 5 minutes
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_USERNAME): cv.string,
+    vol.Required(CONF_USERNAME): cv.string,
+    vol.Optional(CONF_NAME, default={CONF_TYPE: name}): cv.string,
     vol.Optional(CONF_DISPLAY_OPTIONS, default={CONF_TYPE: DEFAULT_TYPE_}):
         vol.All(
             cv.ensure_list, [vol.In(SENSOR_TYPES)]
@@ -34,9 +37,10 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the system monitor sensors."""
-    global customerId
+    global customerId, name
     devices = []
     customerId = config.get(CONF_USERNAME)     
+    name = config.get(CONF_NAME)
     for variable in config[CONF_DISPLAY_OPTIONS]:
         device = evnhanoiclass(variable)
         devices.append(device)
@@ -47,12 +51,12 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 class evnhanoiclass(Entity):
 
     def __init__(self, sensor_type):
-        global customerId
-        self._name = SENSOR_TYPES[sensor_type][0]
+        global customerId, name
+        self._name = SENSOR_TYPES[sensor_type][0] + name
         self.type = sensor_type
         self._state = None
         self._author = 'Tiến Dũng: GooseCapital'
-        self._description = 'Version 1.0 - 27-04-2020'
+        self._description = 'Version 1.1 - 29-04-2020'
         self.update()
         
     @property
@@ -76,14 +80,14 @@ class evnhanoiclass(Entity):
     def update(self):
         global customerId
         _LOGGER.debug('Ma khach hang: ' + customerId)
-        data_ncov = getdataevn.getlastestmonth(customerId, datetime.datetime.now().year)
+        data_evn = getdataevn.getlastestmonth(customerId, datetime.datetime.now().year)
         if self.type == 'customerId':
-            self._state = data_ncov['customerId']
+            self._state = data_evn['customerId']
         elif self.type == 'month':
-            self._state = data_ncov['month']
+            self._state = data_evn['month']
         elif self.type == 'year':
-            self._state = data_ncov['year']
+            self._state = data_evn['year']
         elif self.type == 'power':
-            self._state = data_ncov['power']
+            self._state = data_evn['power']
         elif self.type == 'totalMoney':
-            self._state = data_ncov['totalMoney']
+            self._state = data_evn['totalMoney']
